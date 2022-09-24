0120909eae4bae4eddddfc2132b950427618efea
Vasyl Khreptak <chreptak12@gmail.com>
2022-09-25 00:34:53 +0300

    Added camera clip slider;

---
+++ Assets/Scripts/UI/CameraClippingSlider.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraClippingSlider : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Slider _slider;
+
+    private Camera _camera;
+    
+    #region MonoBehaviour
+
+    private void OnValidate()
+    {
+        _slider ??= GetComponent<Slider>();
+
+        _camera = Camera.main;
+    }
+
+    private void Awake()
+    {
+        _slider.SetValueWithoutNotify(_camera.farClipPlane);
+    }
+
+    private void OnEnable()
+    {
+        _slider.onValueChanged.AddListener(SetClippingDistance);
+    }
+
+    private void OnDisable()
+    {
+        _slider.onValueChanged.RemoveListener(SetClippingDistance);
+    }
+
+    #endregion
+
+    private void SetClippingDistance(float value)
+    {
+        _camera.farClipPlane = value;
+    }
+}
