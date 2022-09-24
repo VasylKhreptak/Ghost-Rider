12954a60de57d506e5c5f04363a4e1f8e06e23ac
Vasyl Khreptak <chreptak12@gmail.com>
2022-09-24 16:01:43 +0300

    Added fullscreen modes;

---
+++ Assets/Scripts/UI/FullScreenModeDropdown.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using ModestTree;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullScreenModeDropdown : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TMP_Dropdown _dropdown;
+
+    private readonly FullScreenMode[] _screenModes = {
+        FullScreenMode.Windowed,
+        FullScreenMode.MaximizedWindow, 
+        FullScreenMode.ExclusiveFullScreen,
+        FullScreenMode.FullScreenWindow
+    };
+     
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        _dropdown.SetValueWithoutNotify(_screenModes.IndexOf(Screen.fullScreenMode));
+    }
+
+    private void OnValidate()
+    {
+        _dropdown ??= GetComponent<TMP_Dropdown>();
+    }
+
+    private void OnEnable()
+    {
+        _dropdown.onValueChanged.AddListener(SetFullScreenMode);
+    }
+
+    private void OnDisable()
+    {
+        _dropdown.onValueChanged.RemoveListener(SetFullScreenMode);
+    }
+
+    #endregion
+
+    private void SetFullScreenMode(int index)
+    {
+        Screen.fullScreenMode = _screenModes[index];
+    }
+}
