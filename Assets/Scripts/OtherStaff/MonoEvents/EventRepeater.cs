04afb00177f776bee3804693df61d276f6d537cf
Vasyl Khreptak <chreptak12@gmail.com>
2022-11-24 20:53:57 +0200

    Added car shop arrows;

---
+++ Assets/Scripts/OtherStaff/MonoEvents/EventRepeater.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EventRepeater : MonoEvent
+{
+    [Header("Events")]
+    [SerializeField] private MonoEvent _event;
+
+    #region MonoBehaviour
+
+    private void OnEnable()
+    {
+        _event.onMonoCall += Invoke;
+    }
+
+    private void OnDisable()
+    {
+        _event.onMonoCall -= Invoke;
+    }
+
+    #endregion
+}
