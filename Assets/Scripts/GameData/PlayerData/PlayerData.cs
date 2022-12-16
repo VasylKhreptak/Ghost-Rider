1f1498e3431c35c94e13d7fef3174f9ba878ccc5
VasylKhreptak <vasylkhreptak12@gmail.com>
2022-12-16 11:37:40 +0200

    Changed startup money to 3000;

--- Assets/Scripts/GameData/PlayerData/PlayerData.cs
+++ Assets/Scripts/GameData/PlayerData/PlayerData.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 [System.Serializable]
 public class PlayerData
 {
-    public SafeInt money = 8000000;
+    public SafeInt money = 3000;
     public Pools lastCar = Pools.MainCarStellar;
     public List<Pools> boughtCars = new List<Pools>() { Pools.MainCarStellar };
 }
